3fe2fe5caef81741c1519b4896a63a05168ea983
Michael Long <michaeltlong10@gmail.com>
2022-09-15 09:32:52 -0400

    Cleaned up duplication in increasing score functionality

--- app.js
+++ app.js
@@ -1,52 +1,56 @@
 const targetScore = document.querySelector("#target");
 const allScoreFlips = document.querySelectorAll(".digit");
-const homeFlips = document.querySelectorAll(".digit.home");
-const awayFlips = document.querySelectorAll(".digit.away");
+const [homeLeft, homeRight] = document.querySelectorAll(".digit.home");
+const [awayLeft, awayRight] = document.querySelectorAll(".digit.away");
 const setFlip = document.querySelector("#set");
 const reset = document.querySelector("#reset");
 
-function updateScore(flip, playerFlips, opponentFlips) {
-  let curNum = parseInt(flip.innerHTML);
-  if (curNum < 9) {
-    flip.innerHTML = curNum + 1;
+const home = {
+  left: homeLeft,
+  right: homeRight
+}
+const away = {
+  left: awayLeft,
+  right: awayRight
+}
+
+function updateScore(flip, player, opponent) {
+  let curDigit = parseInt(flip.innerHTML);
+  if (curDigit < 9) {
+    flip.innerHTML = curDigit + 1;
   } else {
+    player.left.innerHTML = parseInt(player.left.innerHTML) + 1;
     flip.innerHTML = 0;
   }
-  if (parseInt(playerFlips[0].innerHTML + playerFlips[1].innerHTML) === parseInt(targetScore.value)) {
-    for (let flip of playerFlips) {
+  if (parseInt(player.left.innerHTML + player.right.innerHTML)
+    === parseInt(targetScore.value)) {
+    for (let flip of Object.values(player)) {
       flip.style.backgroundColor = "#38ae38";
     }
-    for (let flip of opponentFlips) {
+    for (let flip of Object.values(opponent)) {
       flip.style.backgroundColor = "#cf2b2b";
     }
     for (let scoreFlip of allScoreFlips) {
       scoreFlip.disabled = true;
     }
-  } else {
-    for (let flip of playerFlips) {
-      flip.style.backgroundColor = "#3385c3";
-    }
   }
 }
 
-for (let flip of homeFlips) {
-  flip.addEventListener("click", () => { updateScore(flip, homeFlips, awayFlips) });
-  flip.addEventListener("contextmenu", (event) => {
-    event.preventDefault();
-    if (parseInt(flip.innerHTML) > 0) {
-      flip.innerHTML = parseInt(flip.innerHTML) - 1;
-    }
-  })
+function decreaseDigit(event, flip, floor) {
+  event.preventDefault();
+  if (parseInt(flip.innerHTML) > floor) {
+    flip.innerHTML = parseInt(flip.innerHTML) - 1;
+  }
 }
 
-for (let flip of awayFlips) {
-  flip.addEventListener("click", () => { updateScore(flip, awayFlips, homeFlips) });
-  flip.addEventListener("contextmenu", (event) => {
-    event.preventDefault();
-    if (parseInt(flip.innerHTML) > 0) {
-      flip.innerHTML = parseInt(flip.innerHTML) - 1;
-    }
-  })
+for (let flip of Object.values(home)) {
+  flip.addEventListener("click", () => { updateScore(flip, home, away) });
+  flip.addEventListener("contextmenu", (event) => { decreaseDigit(event, flip, 0) })
+}
+
+for (let flip of Object.values(away)) {
+  flip.addEventListener("click", () => { updateScore(flip, away, home) });
+  flip.addEventListener("contextmenu", (event) => { decreaseDigit(event, flip, 0) })
 }
 
 setFlip.addEventListener("click", () => {
@@ -57,10 +61,7 @@ setFlip.addEventListener("click", () => {
   }
 });
 setFlip.addEventListener("contextmenu", (event) => {
-  event.preventDefault();
-  if (parseInt(setFlip.innerHTML) > 1) {
-    setFlip.innerHTML = parseInt(setFlip.innerHTML) - 1;
-  }
+  decreaseDigit(event, setFlip, 1)
 })
 
 reset.addEventListener("click", () => {
@@ -68,10 +69,10 @@ reset.addEventListener("click", () => {
     scoreFlip.innerHTML = 0;
     scoreFlip.disabled = false;
   }
-  for (let homeFlip of homeFlips) {
+  for (let homeFlip of Object.values(home)) {
     homeFlip.style.backgroundColor = "#3385c3";
   }
-  for (let awayFlip of awayFlips) {
+  for (let awayFlip of Object.values(away)) {
     awayFlip.style.backgroundColor = "#b93d91";
   }
 })
